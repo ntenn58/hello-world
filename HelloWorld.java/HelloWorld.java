3bab4ca6106ed2f79dc2233ae01cdce6366340c9
ntenn58 <Ntenn58@gmail.com>
2025-05-29 15:47:27 -0400

    Revert "Added that this is my first repository"
    This reverts commit 1502aac3f197a0ca7abe07b983fef173055bd684.

--- HelloWorld.java/HelloWorld.java
+++ HelloWorld.java/HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld
 {
    public static void main(String [] args)
    {
-      System.out.println("Hello World");
-      System.out.print("This is my first repository");
+      System.out.print("Hello World");
    }
 
 }
